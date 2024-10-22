a1139c221d51f169770021f83fa9dfb9114a302e
nabilkrs <nabilkrissane.225@gmail.com>
2024-10-22 17:03:35 +0100

    added some changes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/git', function () {
+    return response()->json(['message' => 'Welcome Here !'], 200);
+});
